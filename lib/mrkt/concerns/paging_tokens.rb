f1423eeb8748263479f90fb38b4ea2f729057d8f
Will Spurgin <will.spurgin@orm-tech.com>
2016-06-03 15:48:59 -0500

    Update parameters to hash to match gem conventions

--- lib/mrkt/concerns/paging_tokens.rb
+++ lib/mrkt/concerns/paging_tokens.rb
@@ -1,6 +1,6 @@
 module Mrkt
   module PagingToken
-    def get_paging_token(since)
+    def get_paging_token(since: nil)
       get("rest/v1/activities/pagingtoken.json", { sinceDatetime: since.strftime("%FT%T%:z") })
     end
   end
