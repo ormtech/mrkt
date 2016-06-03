9223a1e4c7021deaa1bdc9f99c464f11a219431c
Will Spurgin <will.spurgin@orm-tech.com>
2016-06-03 16:22:26 -0500

    Conveniently join ids passed in array format

--- lib/mrkt/concerns/crud_activities.rb
+++ lib/mrkt/concerns/crud_activities.rb
@@ -6,12 +6,19 @@ module Mrkt
 
     def get_activities(activity_type_ids, list_id: nil, next_page_token: nil, batch_size: nil)
       params = {}
-      params[:activityTypeIds] = activity_type_ids
+      ids = if activity_type_ids.is_a? Array
+              activity_type_ids.join(",")
+            elsif activity_type_ids.is_a? String
+              activity_type_ids
+            else
+              raise ArgumentError.new("String or Array expected as first argument")
+            end
+      params[:activityTypeIds] = ids
+      params[:nextPageToken] = next_page_token
 
       params[:listId] = list_id if list_id
 
       params[:batchSize] = batch_size if batch_size
-      params[:nextPageToken] = next_page_token if next_page_token
 
       get("/rest/v1/activities.json", params)
     end
