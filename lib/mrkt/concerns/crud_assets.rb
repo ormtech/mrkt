05dcae121569173be58a4dac6c8705c4a927fedf
Will Spurgin <will.spurgin@orm-tech.com>
2017-01-12 15:13:42 -0600

    Add offset ability for paging

--- lib/mrkt/concerns/crud_assets.rb
+++ lib/mrkt/concerns/crud_assets.rb
@@ -1,10 +1,11 @@
 module Mrkt
   module CrudAssets
-    def get_emails_for_program(program_id, max_return: 200)
+    def get_emails_for_program(program_id, max_return: 200, offset: nil)
       params = {
         :maxReturn => max_return,
         :folder    => %Q({"id":#{ program_id },"type":"Program"})
       }
+      params[:offset] = offset if offset
 
       get("/rest/asset/v1/emails.json", params)
     end
