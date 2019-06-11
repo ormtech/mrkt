3498dca082381fd7ca8f6d96e753892c9b744ea1
Jason Stumbaugh <stumbaughjason@gmail.com>
2019-06-11 14:16:42 -0500

    Add endpoint for retrieving asset files

--- lib/mrkt/concerns/crud_assets.rb
+++ lib/mrkt/concerns/crud_assets.rb
@@ -9,5 +9,16 @@ module Mrkt
 
       get("/rest/asset/v1/emails.json", params)
     end
+
+    def get_files(id, max_return: 200, offset: nil)
+      params = {
+        maxReturn: max_return,
+        folder:    %Q({"id":#{id},"type":"Folder"})
+      }
+
+      params[:offset] = offset if offset
+
+      get("/rest/asset/v1/files.json", params)
+    end
   end
 end
